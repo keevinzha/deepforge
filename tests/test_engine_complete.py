afe0fc86f98915e108ea07fb3543b831a0d8bdca
keevinzha <keevinzha@163.com>
2025-11-06 14:37:13 +0800

    remove test from git tracking

--- tests/test_engine_complete.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-@Time ： 2025/11/6 10:56
-@Auth ： keevinzha
-@File ：test_engine_complete.py
-@IDE ：PyCharm
-"""
--- tests/test_forward.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-@Time ： 2025/11/2 14:23
-@Auth ： keevinzha
-@File ：test_forward.py
-@IDE ：PyCharm
-"""
-import pytest
-
